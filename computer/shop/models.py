8dcc969ba3ddd2dcc9ec18c41c40c52b07db2951
edmond <edmondmulera55@gmail.com>
2024-03-27 08:01:27 +0300

    creating views

--- computer/shop/models.py
+++ computer/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    more = models.CharField(max_length=2500)
+    image = models.FileField(upload_to="projects", blank=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
--- computer/shop/views.py
+++ computer/shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def project_index(request):
+    project = Item.objects.all()
+
+    context = {
+        "project" : project
+    }
+    return render(request, "templates/index.html", context)
